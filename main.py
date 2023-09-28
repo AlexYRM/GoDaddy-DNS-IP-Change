7aef3c7eafe507ba05260d340f6fa03019fae8c8
AlexYRM <alexirimia31@gmail.com>
2023-09-28 12:02:37 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+import write
+
+# set up a blocking scheduler instance.
+scheduler = BackgroundScheduler()
+
+print("Start Scheduling jobs")
+# add a job to the scheduler from "write" to be executed at 11:30 every day.
+scheduler.add_job(func=write.ip_verification, trigger="cron", hour=11, minute=30)
+
+# start the scheduler and run until requested to stop
+scheduler.start()
+
+# keep scheduler running until stopped manually
+while True:
+    pass
