b5ae73310caa5435c0a935ffeab1939ae0d96026
AlexYRM <alexirimia31@gmail.com>
2023-10-03 17:25:20 +0300

    .

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 import write
+import time
 
 # set up a blocking scheduler instance.
 scheduler = BackgroundScheduler()
 
 print("Start Scheduling jobs")
-# add a job to the scheduler from "write" to be executed at 16:30 every day.
-scheduler.add_job(func=write.ip_verification, trigger="cron", hour=16, minute=30)
+# add a job to the scheduler from "write" to be executed at 17:30 every day.
+scheduler.add_job(func=write.ip_verification, trigger="cron", hour=17, minute=30)
 
 # start the scheduler and run until requested to stop
 scheduler.start()
 
 # keep scheduler running until stopped manually
 while True:
-    pass
+    time.sleep(2)
