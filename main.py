7cc6aae67fe929657679e4750eba505796b4db8e
AlexYRM <alexirimia31@gmail.com>
2023-10-12 18:00:06 +0300

    changed to blocking scheduler to remove the while loop

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.background import BlockingScheduler
 import write
 import time
 
 # set up a blocking scheduler instance.
-scheduler = BackgroundScheduler()
+scheduler = BlockingScheduler()
 
 print("Start Scheduling jobs")
 # add a job to the scheduler from "write" to be executed at 09:20 every day.
-scheduler.add_job(func=write.ip_verification, trigger="cron", hour=9, minute=20)
+scheduler.add_job(func=write.ip_verification, trigger="cron", hour=17, minute=59)
 
 # start the scheduler and run until requested to stop
 scheduler.start()
 
-# keep scheduler running until stopped manually
-while True:
-    time.sleep(2)
+# # keep scheduler running until stopped manually
+# while True:
+#     time.sleep(2)
